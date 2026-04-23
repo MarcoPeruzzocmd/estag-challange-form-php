eb6152500c157a683429292b90d91c2a3a66f449
mperuzzo <mperuzzo@softexpert.com>
2026-04-23 08:26:03 -0300

    fix: começando a usar .env para as conexões do banco

--- back/src/alert.php
+++
@@ -1,4 +0,0 @@
-<?php if (isset($_SESSION['error'])): ?>
-    <script>alert(<?= json_encode($_SESSION['error'], JSON_HEX_TAG | JSON_HEX_AMP | JSON_HEX_APOS | JSON_HEX_QUOT) ?>);</script>
-    <?php unset($_SESSION['error']); ?>
-<?php endif; ?>
